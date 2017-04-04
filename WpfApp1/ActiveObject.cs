fc28303dbd98c0a136442f12b4157477b377b921
Rateep Jose <rateep.j@gesinfotek.com>
2017-04-04 16:48:42 -0700

    Added First pass of AO pattern(This uses SlimEvents instead of Monitor or EventPool) (Will need to change the existing code to use Monitor or EventPool)

--- WpfApp1/ActiveObject.cs
+++ WpfApp1/ActiveObject.cs
@@ -12,15 +12,16 @@ namespace WpfApp1
         private string _name;
         private Thread _activeObjectThread;
         private bool _initialized;
+        public IWorkQueue WorkQueue { get; private set; }
 
-        public TimeSpan PollFrequency { get; set; } = TimeSpan.FromMilliseconds(300);
+        public TimeSpan PollFrequency { get; set; } = TimeSpan.FromMilliseconds(50);
 
         #region Constructor and Destructors
         public ActiveObjectPart(string partName)
         {
+            WorkQueue = new CmdQueue();
             _initialized = false;
             _name = partName;
-            _activeObjectThread = new Thread(() => AOThread()) { IsBackground = true, };
         }
 
         ~ActiveObjectPart() { Dispose(false); }
@@ -32,7 +33,7 @@ namespace WpfApp1
         {
             if (_disposed) { return; }
 
-            if (disposing){ Uninitialize(); }
+            if (disposing) { Uninitialize(); }
 
             _disposed = true;
         }
@@ -46,21 +47,41 @@ namespace WpfApp1
             while (_runThread)
             {
                 Thread.Sleep(PollFrequency);
-                OnPoll();
+                try
+                {
+                    RunQueuedCommandsIfAny();
+                    OnService();
+                }
+                catch (Exception ex) { System.Diagnostics.Trace.WriteLine(ex.Message); }
             }
         }
 
-        protected virtual void OnPoll() { }
+        private void RunQueuedCommandsIfAny()
+        {
+            if (WorkQueue.Empty) { return; }
+            ICommand cmd;
+            WorkQueue.Pop(out cmd);
+            cmd.RunCmdFunc();
+        }
 
-        public string Initialize()
+        protected virtual void OnService() { }
+
+        public string Start()
+        {
+            try { _activeObjectThread.Start(); } catch (Exception ex) { return ex.Message; }
+            return string.Empty;
+        }
+
+        public string Initialize(bool autoStart = true)
         {
             string ec = string.Empty;
             try
             {
-                if (_initialized) { ec = "The part=['{0}'] is already initialized".format(_name); return ec; }
+                if (_initialized) { ec = "The part=['{0}'] is already initialized".FormatEx(_name); return ec; }
 
+                _activeObjectThread = new Thread(() => AOThread()) { IsBackground = true, };
                 _runThread = true;
-                _activeObjectThread.Start();
+                if (autoStart) { ec = Start(); }
                 return string.Empty;
             }
             catch (Exception ex) { ec = ex.Message; return ec; }
@@ -70,14 +91,150 @@ namespace WpfApp1
         private string Uninitialize()
         {
             _runThread = false;
-            try { _activeObjectThread?.Join(); } catch { }
+            try
+            {
+                if (_activeObjectThread?.IsAlive == true)
+                {
+                    _activeObjectThread.Join(TimeSpan.FromMilliseconds(1000));
+                    if (_activeObjectThread.IsAlive) { _activeObjectThread.Abort(); }
+                }
+            }
+            catch { }
+            finally { _activeObjectThread = null; }
             return string.Empty;
         }
+
+        public ICommandProxy CreateCommand(string cmdName, Func<string> func) => new Command(cmdName, func, WorkQueue);
+
+
+        #region Command class
+        private class Command : ICommand
+        {
+            #region CommandStatus
+            public bool IsComplete => _cmdExectutionStatus == CommandExecutionStatus.Completed;
+
+            public bool IsProcessing => _cmdExectutionStatus == CommandExecutionStatus.Processing;
+
+            public bool IsQueued => _cmdExectutionStatus == CommandExecutionStatus.Queued;
+
+            private string _errorCode;
+            public string ErrorCode { get { return _errorCode; } }
+
+            private ManualResetEventSlim _commandCompletedEvent = new ManualResetEventSlim();
+            public string WaitForCompletion()
+            {
+                if (_cmdExectutionStatus == CommandExecutionStatus.Completed) { return ErrorCode; }
+                try { _commandCompletedEvent.Wait(); } catch { }
+                return ErrorCode;
+            }
+            #endregion
+
+            #region CommandProxy
+            public string Run() => PackAndEnqueue().WaitForCompletion();
+
+            public ICommandStatus Start() => PackAndEnqueue();
+
+            private ICommandStatus PackAndEnqueue()
+            {
+                CommandExecutionStatus ces = _cmdExectutionStatus;
+                if (!(ces == CommandExecutionStatus.Completed
+                    || ces == CommandExecutionStatus.NotQueued)) { return this; }
+
+                _commandCompletedEvent.Reset();
+                _cmdExectutionStatus = CommandExecutionStatus.Queued;
+                return _queue.Push(this);
+            }
+            #endregion
+
+            public string CmdName { get; private set; }
+            public Func<string> CmdFunc { get; private set; }
+            private volatile CommandExecutionStatus _cmdExectutionStatus = CommandExecutionStatus.NotQueued;
+            private IWorkQueue _queue;
+            public Command(string cmdName, Func<string> func, IWorkQueue queue) { CmdName = cmdName; CmdFunc = func; _queue = queue; }
+
+            public void RunCmdFunc()
+            {
+                _errorCode = string.Empty;
+                _cmdExectutionStatus = CommandExecutionStatus.Processing;
+                try { _errorCode = CmdFunc(); }
+                catch(Exception ex) { _errorCode = ex.Message; }
+                finally
+                {
+                    _cmdExectutionStatus = CommandExecutionStatus.Completed;
+                    _commandCompletedEvent.Set();
+                }
+            }
+        }
+        #endregion
+    }
+
+
+    public enum CommandExecutionStatus : int
+    {
+        NotQueued,
+        Queued,
+        Processing,
+        Completed,
+    }
+
+    public interface ICommandStatus
+    {
+        bool IsComplete { get; }
+        bool IsProcessing { get; }
+        bool IsQueued { get; }
+        string ErrorCode { get; }
+        string WaitForCompletion();
+    }
+
+    public interface ICommandProxy
+    {
+        string Run();
+        ICommandStatus Start();
+    }
+
+    public interface ICommandInternal
+    {
+        string CmdName { get; }
+        Func<string> CmdFunc { get; }
+        void RunCmdFunc();
+    }
+
+    public interface ICommand : ICommandProxy, ICommandStatus, ICommandInternal { }
+
+
+    public interface IWorkQueue
+    {
+        bool Empty { get; }
+        ICommandStatus Push(ICommand command);
+        string Pop(out ICommand command);
+    }
+
+    public class CmdQueue : IWorkQueue
+    {
+        private object _lock;
+        private Queue<ICommand> _queue;
+
+        public CmdQueue()
+        {
+            _lock = new object();
+            _queue = new Queue<ICommand>();
+        }
+
+        public bool Empty { get { lock (_lock) { return _queue.Count == 0; } } }
+
+        public string Pop(out ICommand command)
+        {
+            command = null;
+            lock (_lock) { try { command = _queue.Dequeue(); } catch (Exception ex) { return ex.Message; } }
+            return string.Empty;
+        }
+
+        public ICommandStatus Push(ICommand command) { lock (_lock) { _queue.Enqueue(command); } return command; }
     }
 
     public static class Extensions
     {
         public static bool IsNullOrEmpty(this string str) { return string.IsNullOrEmpty(str); }
-        public static string format(this string str, params object[] args) { return string.Format(str, args); }
+        public static string FormatEx(this string str, params object[] args) { return string.Format(str, args); }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -20,9 +21,51 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ActiveObjectPart _aop;
+        DispatcherTimer _dt;
         public MainWindow()
         {
             InitializeComponent();
+            _aop = new ActiveObjectPart("test") { PollFrequency = TimeSpan.FromMilliseconds(100), };
+            string ec = _aop.Initialize();
+            _dt = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(333) };
+            _dt.Tick += UI_Update;
+            _dt.Start();
+        }
+
+        private List<ICommandStatus> _commands = new List<ICommandStatus>(100);
+        private void UI_Update(object sender, EventArgs e)
+        {
+            _data.Text = _multiCommandResult;
+            //dataGrid.ItemsSource = _commands.ToArray();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _aop?.Dispose();
+            _aop = null;
+        }
+
+        private int _index = 0;
+        ICommandProxy icp;
+        private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            int i = ++_index;
+            _dataInit.Text = _index.ToString();
+            //_commands.Add(_aop.CreateCommand("MULTICOMMAND", () => RunCommand(i)).Start());
+            if (icp == null) { icp = _aop.CreateCommand("MULTICOMMAND", () => RunCommand(_index)); _commands.Add(icp.Start()); }
+            else { icp.Start(); }
+            System.Diagnostics.Trace.WriteLine("Test_Click");
+        }
+
+        public string _multiCommandResult { get; set; } = "Empty";
+        private string RunCommand(int i)
+        {
+            for (long j = 0; j < 1; ++j)
+                for (long k = 0; k < 500; ++k) { }
+            _multiCommandResult =  "MultiCommand: Index ={0}".FormatEx(i);
+            System.Diagnostics.Trace.WriteLine(_multiCommandResult);
+            return _multiCommandResult;
         }
     }
 }
