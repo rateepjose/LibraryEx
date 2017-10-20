8d31f32cd6a264cc81c9d180a9658a7f055bb4c9
Rateep Jose <rateep.j@gesinfotek.com>
2017-10-19 18:12:00 -0700

    Added reverse lookup logic for linking each reservation to a set of commands. This helps reduce cpu load in poll operation. Integrated the CommandDispatchManager with the ModelObserverCollection class. (Testing is pending)

--- LibraryEx/CommandDispatcher.cs
+++ LibraryEx/CommandDispatcher.cs
@@ -47,54 +47,96 @@ namespace LibraryEx
             foreach (var runningCmd in _runningCommands) { if (runningCmd.Value.IsComplete) { _finishedRunningCmds.Add(runningCmd.Key); } }
             foreach (var finishedRunningCmd in _finishedRunningCmds)
             {
-                var reservations = _commandAndReservationTable[finishedRunningCmd].reservations;
+                var reservations = _commandToReservationsMap[finishedRunningCmd].reservations;
                 SetReservationsTo(string.Empty, reservations);
+                //Evaluate the Commands based on the reservation changes
+                EvaluateCommandDisableReason(reservations);
+
                 _runningCommands.Remove(finishedRunningCmd);
             }
+            _finishedRunningCmds.Clear();
         }
 
-        private Dictionary<string, (ICommandDispatchClient client,string[] reservations)> _commandAndReservationTable = new Dictionary<string, (ICommandDispatchClient, string[])>();
-        private Dictionary<string, string> _reservationAndReasonMap = new Dictionary<string, string>();
+        private Dictionary<string, (ICommandDispatchClient client,string[] reservations)> _commandToReservationsMap = new Dictionary<string, (ICommandDispatchClient, string[])>();
+        private Dictionary<string, string> _reservationToReasonMap = new Dictionary<string, string>();
+        private Dictionary<string, string[]> _reservationToCommandsMap = new Dictionary<string, string[]>();
+        private Dictionary<string, RefObjectPublisher<string>> _commandToDisableReasonMap = new Dictionary<string, RefObjectPublisher<string>>();
         public ICommandProxy HarvestCommands() => _aop.CreateCommand("HarvestCommand", _ => PerformHarvestCommands());
         private string PerformHarvestCommands()
         {
+            _commandToReservationsMap.Clear();
+            _reservationToReasonMap.Clear();
+            _commandToDisableReasonMap.Clear();
+            _reservationToCommandsMap.Clear();
+
             foreach (var client in Clients)
             {
                 foreach (var kvp in client.CommandReservationTable)
                 {
-                    _commandAndReservationTable[$"{client.Name}.{kvp.Key}"] = (client, kvp.Value);
-                    foreach (var reservation in kvp.Value) { _reservationAndReasonMap[reservation] = string.Empty; }
+                    string fullNameCommand = $"{client.Name}.{kvp.Key}";
+                    //Create a new table(one to many) with fullcommandName(controller+command) & 'tuple of reservationsList and dispatchClient'
+                    _commandToReservationsMap[fullNameCommand] = (client, kvp.Value);
+                    //Create a new table(one to one) with reservation & reason map
+                    foreach (var reservation in kvp.Value) { _reservationToReasonMap[reservation] = string.Empty; }
+                    //Create a new table(one to one) with command & DisableReason
+                    _commandToDisableReasonMap[fullNameCommand] = new RefObjectPublisher<string>() { Object = string.Empty };
                 }
             }
+            //Create a new table(one to many) (for reverse lookup) with reservation & commands
+            _reservationToCommandsMap = _reservationToReasonMap.ToDictionary(a => a.Key, b => _commandToReservationsMap.Where(x => x.Value.reservations.Contains(b.Key)).Select(y => y.Key).ToArray());
+            //Add the same to the ModelObserverCollection so that UI can subscribe if required
+            ModelObserverCollection.AddModelObserverToCollection(_commandToDisableReasonMap.ToDictionary(a => a.Key, b => new RefObjectObserver<string>(b.Value) as IRefObjectObserver));
             return string.Empty;
         }
 
         private void SetReservationsTo(string reservationReason, string[] reservations)
         {
             if (reservations == null) return;
-            foreach (var reservation in reservations) { _reservationAndReasonMap[reservation] = reservationReason; }
+            foreach (var reservation in reservations) { _reservationToReasonMap[reservation] = reservationReason; }
         }
 
         private Dictionary<string, ICommandStatus> _runningCommands = new Dictionary<string, ICommandStatus>();
         public ICommandProxy DispatchCommand(string name, string command, Dictionary<string, string> parameters) => _aop.CreateCommand("DispatchCommand", x => PerformDispatchCommand(name, command, parameters, x));
         private string PerformDispatchCommand(string name, string command, Dictionary<string, string> parameters, ICommandParams commandParams)
         {
-            string nameCommandKey = $"{name}.{command}";
+            string fullNameCommand = $"{name}.{command}";
             //Check if the command is already in the runningCommands list
-            if (!_runningCommands.ContainsKey(nameCommandKey)) return $"Command '{command}' registered by {name} is currently running";
+            if (!_runningCommands.ContainsKey(fullNameCommand)) return $"Command '{command}' registered by {name} is currently running";
 
             //lookup for the command
-            if (!_commandAndReservationTable.TryGetValue(nameCommandKey, out var resTable)) { return $"Command '{command}' not found in '{name}'"; }
+            if (!_commandToReservationsMap.TryGetValue(fullNameCommand, out var resTable)) { return $"Command '{command}' not found in '{name}'"; }
 
             //Check if reservations for the given command are free
-            if (!resTable.reservations.All((x) => _reservationAndReasonMap[x].IsNullOrEmpty())) return $"Command '{command}' cannot be executed since some resources are reserved.(Reserved items:'{string.Join(",", resTable.reservations.Where(x => !_reservationAndReasonMap[x].IsNullOrEmpty()).Select(y => $"[{y}={_reservationAndReasonMap[y]}]").ToArray())}')";
+            if (!resTable.reservations.All((x) => _reservationToReasonMap[x].IsNullOrEmpty())) return $"Command '{command}' cannot be executed since some resources are reserved.(Reserved items:'{string.Join(",", resTable.reservations.Where(x => !_reservationToReasonMap[x].IsNullOrEmpty()).Select(y => $"[{y}={_reservationToReasonMap[y]}]").ToArray())}')";
 
             //Set all the reservations for the given command to busy
-            SetReservationsTo(nameCommandKey, resTable.reservations);
+            SetReservationsTo(fullNameCommand, resTable.reservations);
+
+            //Evaluate the Commands based on the reservation changes
+            EvaluateCommandDisableReason(resTable.reservations);
 
             //Add the command to runningCommands
-            commandParams.SetOutputParams(new DispatcherOutputParams() { CommandStatus = _runningCommands[nameCommandKey] = resTable.client.StartCommand(command, parameters).Start() });
+            commandParams.SetOutputParams(new DispatcherOutputParams() { CommandStatus = _runningCommands[fullNameCommand] = resTable.client.StartCommand(command, parameters).Start() });
             return string.Empty;
+        }
+
+        private void EvaluateCommandDisableReason(string[] reservations)
+        {
+            List<string> commands = new List<string>();
+            foreach (string reservation in reservations) { commands.AddRange(_reservationToCommandsMap[reservation]); }
+
+            StringBuilder disableReason = new StringBuilder(string.Empty);
+            foreach (string command in commands)
+            {
+                disableReason.Clear();
+                string[] cmdReservations = _commandToReservationsMap[command].reservations.ToArray();
+                int itemIndex = 1;
+                foreach (string cmdReservation in cmdReservations)
+                {
+                    if (!cmdReservation.IsNullOrEmpty()) { disableReason.Append($"{itemIndex++}. {cmdReservation}\n"); }
+                }
+                _commandToDisableReasonMap[command].Object = disableReason.ToString().TrimEnd(new char[] { '\n' });
+            }
         }
     }
 }
